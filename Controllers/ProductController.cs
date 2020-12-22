4c6d623da27fd781ea5ff96ce1fc3390d0101e2d
QuakeCyborg <76210879+QuakeCyborg@users.noreply.github.com>
2020-12-22 11:03:43 +0530

    Add files via upload

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -13,48 +13,50 @@ namespace WebApplication1.Controllers
     public class ProductController : Controller
     {
 
-        SqlConnection con = new SqlConnection();
-        SqlCommand com = new SqlCommand();
-        SqlDataReader dr;
+        //SqlConnection con = new SqlConnection();
+        //SqlCommand com = new SqlCommand();
+        //SqlDataReader dr;
 
         [HttpGet]
-        public ActionResult Prod_Descr(int id=1)
+        public ActionResult Prod_Descr(string Prod_ID, string Prod_Name, string Prod_Descr,string Prod_Price)
         {
 
-            Product xx = new Product();
-            xx.kkkk = xx.Table();
-            xx.Product_of_ID = id;
-            return View(xx);
+            Product details = new Product();
+            details.Prod_ID = Prod_ID;
+            details.Prod_Name = Prod_Name;
+            details.Prod_Descr = Prod_Descr;
+            details.Prod_Price = Prod_Price;
+            return View(details);
         }
-        void connectionString()
-        {
+        //void connectionString()
+        //{
 
-            con.ConnectionString = "data source= 192.168.42.1,1433; database= MerchandiseDB;  User ID=cpfuser;Password=!Log123#;";
-        }
+        //    con.ConnectionString = "data source= 192.168.42.1,1433; database= MerchandiseDB;  User ID=cpfuser;Password=!Log123#;";
+        //}
 
         
         
         
         // GET: Product
-        [HttpPost]
+        //[HttpPost]
         
-        public ActionResult Add_to_cart(Product prod)
-        {
-            if (prod.Order_Amount >= 1)
-            {
-                return View();
-            }
-            else
-            {
-                connectionString();
-                con.Open();
-                com.Connection = con;
-                com.CommandText = ""; // Sql to add 1 to order amout
-                dr = com.ExecuteReader();
-                return View();
-            }
+        //public ActionResult Add_to_cart(Product prod)
+        //{
+        //    if (prod.Order_Amount >= 1)
+        //    {
+        //        return View();
+        //    }
+        //    else
+        //    {
+        //        connectionString();
+        //        con.Open();
+        //        com.Connection = con;
+        //        com.CommandText = ""; // Sql to add 1 to order amout
+        //        dr = com.ExecuteReader();
+        //        return View();
+        //    }
 
-        }
+        //}
 
         //public ActionResult Checkout()
         //{
