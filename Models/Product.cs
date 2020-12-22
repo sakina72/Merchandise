b786d1ed9424289af76c156ded25d4271c908917
QuakeCyborg <76210879+QuakeCyborg@users.noreply.github.com>
2020-12-22 11:05:26 +0530

    Add files via upload

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,39 +6,15 @@ using System.Data;
 
 namespace WebApplication1.Models
 {
-    public class Product:homepagemodel
+    public class Product
     {
-        public DataTable kkkk= new DataTable();
+        public string Prod_ID { get; set; }
+        public string Prod_Name { get; set; }
+        public string Prod_Descr { get; set; }
+        public string Prod_Price { get; set; }
 
-        public int Product_of_ID { get; set; }
 
-        public DataTable Table()
-        {
-            // Create a new DataTable.
-            DataTable table = new DataTable("Product Details");
-            // Declare variables for DataColumn and DataRow objects.
-            //DataColumn column;
-            
 
-            table.Columns.Add("Prod_ID");
-            table.Columns.Add("Prod_Name");
-            table.Columns.Add("Prod_Price");
-            table.Columns.Add("Prod_Descr");
-            int c=10;
-            for (c=1; c<=10; c++) {
-                DataRow row = table.NewRow();
-                row["Prod_ID"] = "Product_ID "+c;
-                row["Prod_Name"] = "Produc_tName "+c;
-                row["Prod_Price"] = "Product_price "+c;
-                row["Prod_Descr"] = "Product_Details "+c ;
-
-                table.Rows.Add(row);
-            }
-            
-            return table;
-        }
-
-        
     }
     
 }
