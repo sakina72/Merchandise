658e32f62aba6313ba92edabd7141a0ca137aff3
QuakeCyborg <76210879+QuakeCyborg@users.noreply.github.com>
2020-12-24 14:43:40 +0530

    Add files via upload

---
+++ Models/Sign_Up.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Sign_Up
+    {
+
+        public string New_CustID { get; set; }
+        public string New_Cust_Password { get; set; }
+        public int New_Cust_Contact { get; set; }
+    }
+}
